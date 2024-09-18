b78cb3fc9c21246f3eda20f12d27f41361bfddc6
Rashik-metadrob <muhammad@metadrob.com>
2024-09-18 19:51:42 +0530

    the url changed to uat

--- src/Utils/Api/Base_Url/axios.baseUrl.ts
+++ src/Utils/Api/Base_Url/axios.baseUrl.ts
@@ -1,3 +1,3 @@
-export const BaseUrl= process.env.REACT_APP_BACKEND_URL
+export const BaseUrl= import.meta.env.VITE_API_BACKEND_URL
 // export const BaseUrl= "https://tracking.metadrob.com:3002/v1"
 
